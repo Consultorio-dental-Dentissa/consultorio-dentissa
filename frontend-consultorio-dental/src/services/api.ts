c8c18a796b3d78e529603498317ce5c5e0730532
Luis Enrique Arroyo <167714819+mr-luis15@users.noreply.github.com>
2026-03-16 22:01:38 -0600

    fix(frontend/api): se corrigió un error al momento de atrapar errores en /services/api.ts (#5)
    * Se corrigió un error al momento de atrapar errores en /service/api.ts
    
    * Se crearon las funciones PUT y DELETE en el archivo de /services/api.ts

--- frontend-consultorio-dental/src/services/api.ts
+++ frontend-consultorio-dental/src/services/api.ts
@@ -2,49 +2,159 @@ import { ApiError } from "../types/ApiError";
 
 const url_api = import.meta.env.VITE_API_URL
 
-
+// POST
 export async function post<T>(endpoint: string, datos: object): Promise<T> {
 
-    const respuesta = await fetch(url_api + endpoint, {
-        method: 'POST',
-        body: JSON.stringify(datos),
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json'
+    try {
+        const respuesta = await fetch(url_api + endpoint, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const respuesta_json = await respuesta.json();
+
+        if (!respuesta.ok) {
+            throw new ApiError(
+                respuesta_json.message,
+                respuesta_json.error,
+                respuesta_json.statusCode
+            );
         }
-    });
 
-    const respuesta_json = await respuesta.json();
+        return respuesta_json;
+    } catch (error) {
 
-    if (!respuesta.ok) {
+        // Si YA es un error controlado tuyo, lo dejas pasar
+        if (error instanceof ApiError) {
+            throw error;
+        }
+
         throw new ApiError(
-            respuesta_json.message,
-            respuesta_json.error,
-            respuesta_json.statusCode
+            'Hubo un error al comunicarse con el servidor',
+            'Internal Server Error',
+            500
         );
     }
 
-    return respuesta_json;
+}
+
+// GET
+export async function get<T>(endpoint: string, id?: number): Promise<T> {
+
+    try {
+        const path = id ? `${endpoint}/${id}` : endpoint;
+
+        const respuesta = await fetch(url_api + path, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const respuesta_json = await respuesta.json();
+
+        if (!respuesta.ok) {
+            throw respuesta_json;
+        }
+
+        return respuesta_json;
+        
+    } catch(error) {
+
+        // Si YA es un error controlado tuyo, lo dejas pasar
+        if (error instanceof ApiError) {
+            throw error;
+        }
+        throw new ApiError(
+            'Hubo un error al comunicarse con el servidor',
+            'Internal Server Error',
+            500
+        );
+    }
+
 }
 
 
-export async function get<T>(endpoint: string, id?: number): Promise<T> {
 
-    const path = id ? `${endpoint}/${id}` : endpoint;
 
-    const respuesta = await fetch(url_api + path, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json'
+
+// PUT
+export async function put<T>(endpoint: string, id: number, datos: object): Promise<T> {
+
+    try {
+        const respuesta = await fetch(`${url_api}${endpoint}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(datos),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const respuesta_json = await respuesta.json();
+
+        if (!respuesta.ok) {
+            throw new ApiError(
+                respuesta_json.message,
+                respuesta_json.error,
+                respuesta_json.statusCode
+            );
         }
-    });
 
-    const respuesta_json = await respuesta.json();
+        return respuesta_json;
+    } catch (error) {
 
-    if (!respuesta.ok) {
-        throw respuesta_json;
+        if (error instanceof ApiError) {
+            throw error;
+        }
+
+        throw new ApiError(
+            'Hubo un error al comunicarse con el servidor',
+            'Internal Server Error',
+            500
+        );
     }
+}
 
-    return respuesta_json;
+
+// DELETE
+export async function deleteR<T>(endpoint: string, id: number): Promise<T> {
+
+    try {
+        const respuesta = await fetch(`${url_api}${endpoint}/${id}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const respuesta_json = await respuesta.json();
+
+        if (!respuesta.ok) {
+            throw new ApiError(
+                respuesta_json.message,
+                respuesta_json.error,
+                respuesta_json.statusCode
+            );
+        }
+
+        return respuesta_json;
+    } catch (error) {
+
+        if (error instanceof ApiError) {
+            throw error;
+        }
+
+        throw new ApiError(
+            'Hubo un error al comunicarse con el servidor',
+            'Internal Server Error',
+            500
+        );
+    }
 }
