f775fee06f171c3e030161817f08873ceaf721b8
Luis Enrique Arroyo <167714819+mr-luis15@users.noreply.github.com>
2026-03-25 17:51:19 -0600

    Se integró la base de datos con postgresql (#15)

--- backend-consultorio-dental/src/infrastructure/prisma/prisma.service.ts
+++ backend-consultorio-dental/src/infrastructure/prisma/prisma.service.ts
@@ -1,46 +1,32 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMssql } from '@prisma/adapter-mssql';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
 
   constructor() {
+    const connectionString = process.env.DATABASE_URL;
 
-    const server = process.env["DATABASE_HOST"];
-    const database = process.env["DATABASE_NAME"];
-    const user = process.env["DATABASE_USER"];
-    const password = process.env["DATABASE_PASSWORD"];
-
-    if (!server || !database || !user || !password) {
-      throw new Error(
-        'Faltan variables de entorno para la conexión a SQL Server. ' +
-        'Verifica DB_HOST, DB_NAME, DB_USER y DB_PASSWORD en tu .env'
-      );
+    if (!connectionString) {
+      throw new Error('DATABASE_URL no definida');
     }
 
-    const adapter = new PrismaMssql({
-      server: server,
-      port: Number(process.env["DATABASE_PORT"]) || 1433,
-      database: database,
-      user: user,
-      password: password,
-      pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000,
-      },
-      options: {
-        encrypt: true,
-        trustServerCertificate: true,
-      },
+    const adapter = new PrismaPg({
+      connectionString,
     });
 
-    super({ adapter, omit: { usuario: { contraseña: true}} });
+    super({
+      adapter,
+      log: ['error', 'warn'],
+      omit: {
+        usuario: {
+          contraseña: true,
+        },
+      },
 
-
-
+    });
   }
 
   async onModuleInit() {
