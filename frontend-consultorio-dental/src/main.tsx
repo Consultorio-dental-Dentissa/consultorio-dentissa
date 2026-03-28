c52012bb7350b7ce0b837e20c2c87aa4bc32d0f2
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-03-28 15:54:22 -0600

    Desinstalamos la libreria  de material-ui y sus dependencias

--- frontend-consultorio-dental/src/main.tsx
+++ frontend-consultorio-dental/src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
 import App from './App'
-
-const theme = createTheme() // tema por defecto, lo personalizamos después
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* resetea estilos del navegador */}
       <App />
-    </ThemeProvider>
   </StrictMode>
 )
