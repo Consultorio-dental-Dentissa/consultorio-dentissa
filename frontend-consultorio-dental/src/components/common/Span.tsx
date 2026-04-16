4d4b4c5345cad62513ce653b2b51ce046f4f9ea2
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-16 10:06:46 -0600

    feat:(frontend/components): a new StatusSpan component has been created in Span.tsx file

--- frontend-consultorio-dental/src/components/common/Span.tsx
+++ frontend-consultorio-dental/src/components/common/Span.tsx
@@ -1,10 +1,59 @@
+import { StatusAppointment } from "@/types/enums/status-appointment.enum";
 
-interface ErrorSpanProps {
+interface SpanProps {
     message: string
 }
 
-export function ErrorSpan({message}: ErrorSpanProps) {
+export function ErrorSpan({message}: SpanProps) {
     return(
         <span className="text-red-500">{message}</span>
     );
+}
+
+interface StatusSpanProps {
+    status: string;
+}
+
+export function StatusSpan({ status }: StatusSpanProps) {
+    
+    let styles: {backgroundColor: string; textColor: string; };
+
+    switch(status) {
+        case StatusAppointment.PENDIENTE:
+            styles = {
+                backgroundColor: 'bg-gray-500',
+                textColor: 'text-white'
+            }
+            break;
+        case StatusAppointment.CONFIRMADA:
+            styles = {
+                backgroundColor: 'bg-green-200',
+                textColor: 'text-green-700'
+            }
+            break;
+        case StatusAppointment.CANCELADA:
+            styles = {
+                backgroundColor: 'bg-red-200',
+                textColor: 'text-red-600'
+            }
+            break;
+        case StatusAppointment.REPROGRAMADA: {
+            styles = {
+                backgroundColor: 'bg-yellow-200',
+                textColor: 'text-yellow-700'
+            }
+            break;
+        }
+
+        default:
+            styles = {
+                backgroundColor: 'bg-gray-400',
+                textColor: 'bg-gray-600'
+            }
+            break;
+    }
+    
+    return (
+        <span className={`py-1 rounded-full font-medium px-3 ${styles.backgroundColor} ${styles.textColor}`}> { status.toLowerCase().charAt(0).toUpperCase() + status.slice(1).toLowerCase() } </span>
+    )
 }
