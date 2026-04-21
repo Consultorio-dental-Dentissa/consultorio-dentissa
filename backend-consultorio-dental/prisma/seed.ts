972f28c02b87aa94e0c5b47ead50d8e45f3fdb49
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-21 17:39:36 -0600

    feat(backend/database): add roles to seed script

--- backend-consultorio-dental/prisma/seed.ts
+++ backend-consultorio-dental/prisma/seed.ts
@@ -1,9 +1,33 @@
 import { PrismaService } from "src/infrastructure/prisma/prisma.service";
+import { Role } from "src/modules/users/enums/rol.enum";
 
 const prisma = new PrismaService();
 
 async function main() {
-    
+
+    const rolesArray = Object.values(Role);
+    const roles = rolesArray.map(role => {
+        return {
+            role: role
+        }
+    });
+
+    const savedRoles = await prisma.role.createMany({
+        data: roles,
+        skipDuplicates: true
+    });
+
+    if (savedRoles.count > 0) {
+        console.log("Success: roles created successfully");
+
+    } else {
+        console.log("Info: roles already exitst");
+    }
 }
 
-main().finally(() => prisma.$disconnect());
+main()
+    .catch(error => {
+        console.log("Error: ", error);
+        process.exit(1);
+    })
+    .finally(() => prisma.$disconnect());
