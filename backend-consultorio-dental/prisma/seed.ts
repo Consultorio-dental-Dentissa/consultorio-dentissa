f380a74a5bc876772f0465042a1dd2994672e208
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-21 11:11:56 -0600

    feat(backend/database): add roles to seed

--- backend-consultorio-dental/prisma/seed.ts
+++ backend-consultorio-dental/prisma/seed.ts
@@ -1,9 +1,27 @@
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
+import { Role } from 'src/modules/users/enums/rol.enum';
 
 const prisma = new PrismaService();
 
 async function main() {
+    const rolesArray = Object.values(Role);
+    const roles = rolesArray.map(role => {
+        return {
+            role: role
+        }
+    });
 
+    const createdRoles = await prisma.role.createMany({
+        data: roles,
+        skipDuplicates: true
+    });
+
+    if (createdRoles.count > 0) {
+        console.log("Success: roles created successfully");
+
+    } else {
+        console.log("Info: roles already exist");
+    }
 }
 
 main()
