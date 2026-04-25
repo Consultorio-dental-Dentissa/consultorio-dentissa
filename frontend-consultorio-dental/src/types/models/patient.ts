97c12e85c5624cfa70d780f21b25f8a68a9aaa76
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-25 12:01:06 -0600

    feat(frontend/modules:patient): add patient model and mapper

---
+++ frontend-consultorio-dental/src/types/models/patient.ts
@@ -0,0 +1,10 @@
+export interface Patient {
+    id: number;
+    address: string;
+    birth_date: Date;
+    emergency_phone: string;
+    name: string,
+    lastname: string,
+    email: string,
+    phone: string
+}
