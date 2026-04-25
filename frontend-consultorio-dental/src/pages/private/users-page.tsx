4cc43c484b49e385281927cb1f402b2b94cc8ba4
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-25 15:24:47 -0600

    feat(frontend/users-page): implement data table component in users page

--- frontend-consultorio-dental/src/pages/private/users-page.tsx
+++ frontend-consultorio-dental/src/pages/private/users-page.tsx
@@ -2,33 +2,38 @@ import { useEffect, useState } from "react";
 import { useUsers } from "../../hooks/use-users";
 
 import { PageTitle } from "../../components/common/page-title.component";
-import EmptyTable from "../../components/common/empty-table.component";
 import { CreateUserModal } from "@/components/users/create-user-modal.component";
 import { Button } from "@/components/ui/button"
-import { Switch } from "@/components/ui/switch";
+import { DataTable } from "@/components/common/data-table.component";
 
-import { formatFirstLetterUppercase, formatPhone } from "@/utils/formatters";
 import toast from "react-hot-toast";
 
-import type { UserResponse } from "@/types/api/responses/user.response";
 import type { User } from "@/types/models/user";
 import type { CreateUserDto } from "@/types/api/request/create-user.dto";
+import { getColumns } from "@/components/users/data-table-colums.component";
 
 export default function UsersPage() {
 
     const [users, setUsers] = useState<User[]>([])
     const [openModal, setOpenModal] = useState(false);
+    const [loadingTable, setLoadingTable] = useState(false);
 
-    const { getUsers, updateUserStatus, registerUser, loadingTable } = useUsers();
+    const { getUsers, updateUserStatus, registerUser } = useUsers();
 
     useEffect(() => {
 
         async function fetchUsers() {
             try {
+                setLoadingTable(true);
                 const users = await getUsers();
                 setUsers(users);
+                
             } catch (error) {
                 toast.error((error as string))
+
+            }
+            finally {
+                setLoadingTable(false);
             }
         }
 
@@ -36,31 +41,32 @@ export default function UsersPage() {
     }, []);
 
 
-    const handleNewUser = async (user: CreateUserDto) => {
+    const handleAddUser = async (userData: CreateUserDto) => {
         try {
-            const newUser = await registerUser(user);
-            setUsers(prev => [...prev, newUser]);
+            const user = await registerUser(userData);
+            setUsers(prev => [...prev, user]);
             setOpenModal(false);
-            toast.success('Se ha creado un nuevo usuario');
+            toast.success(`El usuario ${user.name} ha sido registrado correctamente.`);
 
         } catch (error) {
             toast.error((error as string));
         }
     }
 
-    const handleUpdatedUserStatus = async (id: number, newStatus: boolean) => {
-
+    const handleUpdatedUserStatus = async (id: number, status: boolean) => {
         try {
-            await updateUserStatus(id, newStatus);
-            setUsers(prev => {
-                return prev.map(u => u.id === id ? { ...u, status: newStatus } : u)
-            });
+            const statusRes = await updateUserStatus(id, status);
+            if (statusRes) {
+                setUsers(prev => prev.map(user => user.id === id ? { ...user, status: status } : user));
+                toast.success('El estado se actualizó correctamente');
+            }
 
-            toast.success('El estado se actualizó correctamente');
         } catch (error) {
             toast.error((error as string))
         }
     }
+
+    const columns = getColumns(handleUpdatedUserStatus);
 
     return (
         <div>
@@ -76,64 +82,33 @@ export default function UsersPage() {
                 </Button>
             </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Teléfono</th>
-                        <th>Correo</th>
-                        <th>Estado</th>
-                        <th>Rol</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
+            <div className="bg-white mt-5">
+                {
+                    loadingTable ? 
+                        <div className="bg-white rounded-sm p-5 flex justify-center">
+                            <h2>Cargando...</h2>
+                        </div>
+                        :
+                    users.length > 0 ?
+                        <DataTable
+                            columns={columns}
+                            data={users}
+                        />
+                        :
+                    !loadingTable && !users.length ?
+                        <div className="bg-white rounded-sm p-5 flex justify-center">
+                            <h2>No se encontrarón usuarios.</h2>
+                        </div>
+                        :
+                        ''
+                }
 
-                    {
-                        loadingTable ? (
-                            <EmptyTable
-                                mensaje="Cargando..."
-                                submensaje="Buscando usuarios"
-                                colSpan={7}
-                            />
-                        ) : users.length === 0 ? (
-                            <EmptyTable
-                                mensaje="No se encontraron usuarios"
-                                submensaje="Intenta agregar un nuevo usuario"
-                                colSpan={7}
-                            />
-                        ) : (
-                            users.map((usuario) => (
-                                <tr key={usuario.id}>
-                                    <td style={{ fontWeight: '600' }}>{usuario.name}</td>
-                                    <td style={{ fontWeight: '500' }}>{usuario.lastname}</td>
-                                    <td>{formatPhone(usuario.phone)}</td>
-                                    <td>{usuario.email}</td>
-
-                                    <td>
-                                        <Switch checked={usuario.status} onClick={() => handleUpdatedUserStatus(usuario.id, !usuario.status)}/>
-                                    </td>
-
-                                    <td>{formatFirstLetterUppercase(usuario.role)}</td>
-
-                                    <td>
-                                        <div className="actions">
-                                            <Button variant="secondary">Editar</Button>
-                                            <Button variant="destructive">Eliminar</Button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            ))
-                        )
-                    }
-                </tbody>
-            </table>
+            </div>
 
             <CreateUserModal
                 open={openModal}
                 onClose={() => setOpenModal(false)}
-                onSubmit={handleNewUser}
+                onSubmit={handleAddUser}
             />
 
         </div>
