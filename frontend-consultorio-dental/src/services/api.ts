3c995f54d63a5c2494dc9eb39133b0a1573824b9
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-28 23:01:45 -0600

    refactor(frontend/api): prepare interceptor for future standardized api responses and refactor error handler

--- frontend-consultorio-dental/src/services/api.ts
+++ frontend-consultorio-dental/src/services/api.ts
@@ -1,80 +1,81 @@
-import { ApiError } from "../types/api/responses/api-error";
+import type { ApiResponse } from '@/types/api/responses/api.response';
 import axios from 'axios';
 
-const urlAPi = import.meta.env.VITE_API_URL
 const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
-})
+});
+
+api.interceptors.response.use(
+    (response) => {
+
+        /**
+         * Dejamos esto preparado para cuando se implemente
+         * el interceptor de respuestas del backend
+         */
+        if (response.data.success && response.data.data) {
+            const apiResponse: ApiResponse<any> = response.data;
+            console.log("Standar response: ", apiResponse);
+
+            return apiResponse.data;
+        }
+
+        /**
+         * Por ahora solo retornamos la data resultante sin el formato
+         * de ApiResponse hasta agregar el interceptor de respuestas 
+         * en el backend. 
+         */
+        console.log("Not a standar response: ", response.data);
+        return response.data
+    },
+    (error) => {
+
+        if (!axios.isAxiosError(error)) {
+            console.log("Error no esperado");
+
+            throw new Error('Error desconocido');
+        }
+
+        if (error.response?.data.message) {
+            if (error.response?.data.message) {
+                /**
+                 * Dejamos el manejo de errores del backend 
+                 * temporalmente de esta forma hasta implementar
+                 * un contrato de respuestas con los errores 
+                 * de la api en el futuro.
+                 * 
+                 * Por ahora el backend seguirá mandando errores
+                 * con el formato de { message, code, statusCode }
+                 * el cual esta almacenado en error.response.data
+                 */
+                const message = error.response.data.message;
+                throw new Error(message);
+            }
+        }
+
+        console.log("Error al comunicarse con el servidor");
+
+        throw new Error('Error al comunicarse con el servidor');
+    }
+)
+
 
 export async function get<T>(endpoint: string): Promise<T> {
-    try {
-        const response = await api.get(`${urlAPi}${endpoint}`);
-        return response.data;
-
-    } catch (error) {
-        handleError(error);
-    }
+    return api.get(endpoint);
 }
 
 export async function post<T>(endpoint: string, data: object): Promise<T> {
-    try {
-        const response = await api.post(`${urlAPi}${endpoint}`, data);
-        return response.data;
-
-    } catch (error) {
-        handleError(error);
-    }
+    return api.post(endpoint, data);
 }
 
-export async function put<T>() {
-
+export async function put<T>(endpoint: string, data: object): Promise<T> {
+    return api.put(endpoint, data)
 }
 
 export async function patch<T>(endpoint: string, data: object): Promise<T> {
-    try {
-        const response = await api.patch(`${urlAPi}${endpoint}`, data);
-        return response.data;
-
-    } catch (error) {
-        handleError(error);
-    }
+    return api.patch(endpoint, data);
 }
 
 export async function deleteR<T>(endpoint: string): Promise<T> {
-    try {
-        const response = await api.delete(`${urlAPi}${endpoint}`);
-        console.log(response);
-        return response.data;
-
-    } catch (error) {
-        handleError(error);
-    }
-}
-
-
-
-
-function handleError(error: unknown): never {
-    if (axios.isAxiosError(error)) {
-        if (error.response) {
-            throw new ApiError(
-                {
-                    message: error.response.data.message ?? 'Error del servidor',
-                    error: error.response.data.error,
-                    statusCode: error.response.data.statusCode
-                }
-            )
-        } else if (error.request) {
-            throw new ApiError({
-                message: 'No se pudó comunicar con el servidor',
-                error: 'Internal Server Error',
-                statusCode: 500
-            })
-        }
-    }
-    throw new ApiError({
-        message: 'Error inesperado',
-        error: 'Internal Server Error',
-        statusCode: 500
-    })
+    return api.delete(endpoint);
 }
