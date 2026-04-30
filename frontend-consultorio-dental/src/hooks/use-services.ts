d0796e3af50abfd267cafc26858bdcf4a3251a32
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-04-29 18:41:49 -0600

    refactor(frontend/hooks): move data state from pages components to hooks

--- frontend-consultorio-dental/src/hooks/use-services.ts
+++ frontend-consultorio-dental/src/hooks/use-services.ts
@@ -6,37 +6,71 @@ import type { CreateServiceDto } from "../types/api/request/create-service.dto";
 
 export function useServices() {
 
-    const [loading, setLoading] = useState<boolean>(false);
-    const [loadingTable, setLoadingTable] = useState<boolean>(false);
+    const [servicesData, setServicesData] = useState<Service[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
 
-    async function createService(createService: CreateServiceDto): Promise<Service> {
-        
+    async function createService(createServiceDto: CreateServiceDto): Promise<Service | null> {
         setError(null);
-        setLoading(true);
+        setIsLoading(true);
 
-        return await requestCreateService(createService)
-            .catch((error: Error) => {setError(error.message); console.log(error); throw error.message;})
-            .finally(() => setLoading(false));
+        try {
+            const serviceCreated = await requestCreateService(createServiceDto);
+            setServicesData(prev => [...prev, serviceCreated]);
+            return serviceCreated;
+
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+            setError(errorMessage);
+            return null;
+
+        } finally {
+            setIsLoading(false);
+        }
     }
 
-    async function getServices(): Promise<Service[]> {
+    async function getServices() {
 
         setError(null);
-        setLoadingTable(true);
+        setIsLoading(true);
 
-        return await requestGetServices()
-            .catch((error: Error) => {setError(error.message); throw error.message})
-            .finally(() => setLoadingTable(false));
+        try {
+            const users = await requestGetServices();
+            setServicesData(users);
+
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+            setError(errorMessage);
+
+        } finally { setIsLoading(false) }
+
     }
 
     async function updateServiceStatus(id: number, status: boolean): Promise<boolean> {
-        
         setError(null);
+        setIsLoading(true);
 
-        return await requestUpdateServiceStatus(id, status)
-            .catch((error: Error) => {setError(error.message); throw error.message})
+        try {
+            const isStatusUpdated = await requestUpdateServiceStatus(id, status);
+            isStatusUpdated && setServicesData(prev => prev.map(service => service.id === id ? {...service, status} : service));
+            return isStatusUpdated;
+
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+            setError(errorMessage);
+            return false;
+
+        } finally {
+            setIsLoading(false);
+        }
     }
 
-    return { getServices, updateServiceStatus, createService, loading, loadingTable, error }
+    return { 
+        servicesData, 
+        getServices, 
+        updateServiceStatus, 
+        createService, 
+        isLoading, 
+        error 
+    }
 }
