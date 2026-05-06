c5fd752a37559c5c484840377501a1d3dca297c4
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-05-05 19:11:31 -0600

    feat(backend/dto): add more robust validations in dtos

--- backend-consultorio-dental/src/modules/auth/dto/login.dto.ts
+++ backend-consultorio-dental/src/modules/auth/dto/login.dto.ts
@@ -1,12 +1,14 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+import { IsEmail, IsDefined, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginDto {
 
-    @IsEmail({}, { message: 'El correo no es válido' })
-    @IsNotEmpty({message: 'El correo es requerido'})
-    email!: string
+    @IsDefined({ message: "El correo es requerido" })
+    @IsNotEmpty({ message: "El correo es requerido" })
+    @IsEmail({}, { message: "El correo no esta en un formato valido" })
+    email!: string;
 
-    @IsString()
-    @IsNotEmpty({message: 'La contraseña es requerida'})
-    password!: string
+    @IsDefined({ message: "La contraseña es requerida" })
+    @IsNotEmpty({ message: "La contraseña es requerida" })
+    @IsString({ message: "La contraseña debe ser un texto plano" })
+    password!: string;
 }
