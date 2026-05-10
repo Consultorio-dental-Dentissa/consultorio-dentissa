b7fb7be7b2f998f53940154cd11ed31d55e1c6fb
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-05-09 21:20:50 -0600

    feat(frontend/formatters): add minutes to hours formatter

--- frontend-consultorio-dental/src/utils/formatters.ts
+++ frontend-consultorio-dental/src/utils/formatters.ts
@@ -7,3 +7,9 @@ export function formatPhone(phone: string): string {
     phone = phone.trim();
     return `${phone.slice(0, 3)} ${phone.slice(3, 6)} ${phone.slice(6)}`;
 }
+
+export function formatTotalMinutesToHours(totalMinutes: number) {
+    const hours = Math.floor(totalMinutes / 60)
+    const mins = totalMinutes % 60
+    return `${hours}h ${mins}m`
+}
