95483ffffe55bf9a7e40b71b99c31685d339bc02
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-05-15 10:45:55 -0600

    feat(backend/appointments): add get-appointments dto

---
+++ backend-consultorio-dental/src/modules/appointments/dto/get-appointment.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsInt, IsEnum } from 'class-validator';
+import { Type, Transform } from 'class-transformer';
+import { AppointmentStatus } from '@prisma/client';
+
+export class GetAppointmentsDto {
+    @IsOptional()
+    @IsInt({message: 'El ID del paciente debe ser un número'})
+    @Type(() => Number)
+    patient_id?: number;
+
+    @IsOptional()
+    @Transform(({ value }) => value.toUpperCase())
+    @IsEnum(AppointmentStatus, {message: `El estatus debe ser de: ${Object.values(AppointmentStatus)}`})
+    status?: AppointmentStatus
+}
