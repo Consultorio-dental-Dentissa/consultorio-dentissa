19b801a560029b254a48d558539717f1a0635d4f
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-05-26 22:26:50 -0600

    feat(frontent/data-table): change data table component styles

--- frontend-consultorio-dental/src/components/common/page-title.component.tsx
+++ frontend-consultorio-dental/src/components/common/page-title.component.tsx
@@ -9,7 +9,7 @@ export function PageTitle ({titulo, subtitulo} : PageTitleProps) {
     return (
         <div>
             <div className="flex-row">
-                <h4 className="text-2xl font-bold">{titulo}</h4>
+                <h4 className="text-[30px] font-black">{titulo}</h4>
                 <h5 className="text-base text-gray-700">{subtitulo}</h5>
             </div>
         </div>
