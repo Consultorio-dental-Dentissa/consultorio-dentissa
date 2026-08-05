cb3a87db329f5b64ac3c378a91ea645a658699b4
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-08-05 15:53:12 -0600

    chore(frontend): install tanstack react query

--- frontend-consultorio-dental/src/App.tsx
+++ frontend-consultorio-dental/src/App.tsx
@@ -1,17 +1,22 @@
 import Router from './router/router'
 import { AuthProvider } from '@/features/auth/context/auth-context-provider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast'
+
+const queryClient = new QueryClient()
 
 
 function App() {
 
   return (
     <>
-      <Toaster position="top-right" />
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="top-right" />
 
-      <AuthProvider>
-        <Router />
-      </AuthProvider>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </QueryClientProvider>
     </>
   )
 
