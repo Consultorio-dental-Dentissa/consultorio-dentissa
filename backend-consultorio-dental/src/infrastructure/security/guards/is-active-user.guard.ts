d95e1e4b8ed12d88ddbf56d6fd83f1dee6d93b7f
Luis Enrique Arroyo <arroyoluisenrique72@gmail.com>
2026-08-23 13:48:57 -0600

    feat(backend/services): implement public decorator in GET services endpoint and add validation of that decorator in AuthGuard and IsActiveUserGuard

--- backend-consultorio-dental/src/infrastructure/security/guards/is-active-user.guard.ts
+++ backend-consultorio-dental/src/infrastructure/security/guards/is-active-user.guard.ts
@@ -1,12 +1,30 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class IsActiveUserGuard implements CanActivate {
 
-    constructor(private readonly repository: PrismaService) {}
+    constructor(
+        private readonly repository: PrismaService,
+        private reflector: Reflector
+    ) { }
 
     async canActivate(context: ExecutionContext) {
+
+        const isPublic = this.reflector.getAllAndOverride<boolean>(
+            IS_PUBLIC_KEY, 
+            [
+                context.getHandler(),
+                context.getClass(),
+            ]
+        );
+
+
+        if (isPublic) {
+            return true;
+        }
 
         const request = context.switchToHttp().getRequest();
         const status = await this.repository.user.count({
